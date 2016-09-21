1043d4cad10420a28e3b3fe91f141e45cf1dfbdb
INOTECH\karlareyes <kreyes@inotech.mx>
2016-09-20 20:55:37 -0500

    Pantalla Login Parte 2 implentacion de fake-backend

--- app/routesApp.routes.ts
+++ app/routesApp.routes.ts
@@ -3,14 +3,22 @@ import { ParejasDetalleComponent } from './parejadetalle.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { DinamicasComponent } from './dinamicas/dinamicas.component';
+import { UsuariosComponent} from './usuarios/usuarios.component';
+import { AuthGuard } from './login/auth.guard';
 
 
 export const RoutesAppRoutes = [
 //  { path: '',component: HomeComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  //{ path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},  
   { path: 'login', component: LoginComponent},
   { path: 'parejas', component: ParejasListComponent },
   { path: 'pareja/:id', component: ParejasDetalleComponent },
   { path: 'dinamicas', component: DinamicasComponent},
+  { path: 'usuarios',component: UsuariosComponent}
+
+  
+ // { path: '**', redirectTo: '' }
 ]
